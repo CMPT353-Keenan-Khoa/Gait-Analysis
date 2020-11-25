bcf0d6be2a95ee4f40ec444ff07acb41b54eefba
bgh716 <kbyun@sfu.ca>
2020-11-25 06:24:38 -0700

    clean test

--- cleaning.py
+++ cleaning.py
@@ -3,19 +3,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-from statsmodels.nonparametric.smoothers_lowess import lowess
 
 #filename = sys.argv[1]
-filename = 'normal.csv'
+filename = 'pt.csv'
 gait = pd.read_csv(filename)
 columns = gait.columns
-print(columns[0])
 if 'ay (m/s^2)' in columns:
     gait = gait.rename(columns={"ay (m/s^2)": "ay"})
-print(gait)
+
 
 #time decision
-gait = gait.loc[(gait['time']>10)&(gait['time']<60)]
+gait = gait.loc[(gait['time']>60)&(gait['time']<280)]
 
 #butter filter
 b, a = signal.butter(3, 0.05, btype='lowpass', analog=False)
@@ -23,17 +21,24 @@ gait['ay'] = signal.filtfilt(b, a, gait['ay'])
 #plt.plot(gait['time'], gait['ay'], 'b-')
 #plt.show()
 
-#gait['prev'] = gait['gFy'].shift(periods=1)
-gait['next'] = gait['ay'].shift(periods=-1)
-gait = gait[['time','ay','next']]
 
-#gaitmax = gait.loc[(gait['gFy']>gait['prev'])&(gait['gFy']>gait['next'])]
+
+gait['prev'] = gait['ay'].shift(periods=1)
+gait['next'] = gait['ay'].shift(periods=-1)
+gait['timeN'] = gait['time'].shift(periods=-1)
+gait = gait[['time','ay','next', 'timeN', 'prev']]
+gait.dropna(inplace=True)
+
+gait['speed'] = gait['ay'] * (gait['timeN']-gait['time'])
+gait['distance(cm)'] = gait['speed'] * (gait['timeN']-gait['time'])
+gait['distance(cm)'] = gait['distance(cm)'] * 100
+
+#gaitmax = gait.loc[(gait['ay']>gait['prev'])&(gait['ay']>gait['next'])]
 #gaitmin = gait.loc[(gait['gFy']<gait['prev'])&(gait['gFy']<gait['next'])]
-#gaitmax = gaitmax.loc[(gaitmax['time']>60)&(gaitmax['time']<280)]
-#gaitmin = gaitmin.loc[(gaitmin['time']>60)&(gaitmin['time']<280)]
 #gaitmax['ntime'] = gaitmax['time'].shift(periods=-1)
 #gaitmin['ntime'] = gaitmin['time'].shift(periods=-1)
 
+#gait['max'] = np.where(((gait['ay']>gait['prev']) & (gait['ay']>gait['next'])), 1, 0)
 
 gaitleft = gait.loc[((gait['ay']<0)&(gait['next']>0))]
 gaitright =  gait.loc[((gait['ay']>0)&(gait['next']<0))]
@@ -44,12 +49,19 @@ gaitright = gaitright.reset_index()
 #print(gaitright)
 
 
+gait['test'] = 1
+gaittest = gait.loc[(gait['distance(cm)']>0)]
+
+gaittest = gaittest.groupby('test').sum()
+
 gaitright['timeN'] = gaitright['time'].shift(periods=-1)
 gaitleft['timeN'] = gaitleft['time'].shift(periods=-1)
 
-#ave time of one step = 0.42sec
-#ave speed of walking = 178.8 cm/s
-#ave length of one step = 76.2 cm
+
+
+#ave speed of walking = 178.8 cm/s https://www.healthline.com/health/exercise-fitness/average-walking-speed#:~:text=What%20Is%20the%20Average%20Walking%20Speed%20of%20an%20Adult%3F&text=The%20average%20walking%20speed%20of%20a%20human%20is%203%20to,age%2C%20sex%2C%20and%20height.
+#ave length of one step = 76.2 cm https://www.scientificamerican.com/article/bring-science-home-estimating-height-walk/#:~:text=On%20average%2C%20adults%20have%20a,from%20about%200.41%20to%200.45).
+#ave time of one step = 0.42sec calculated from above
 
 
 if gaitleft['time'][0] > gaitright['time'][0]:
@@ -65,9 +77,13 @@ steptimeL.dropna(inplace=True)
 result = pd.concat([steptimeR,steptimeL], axis=1, sort=False, ignore_index=False)
 result = result.rename(columns={"time": "right", 0: "left"})
 
+gaittest['one swing'] = gaittest['distance(cm)'] / len(gaitleft)
+print(gaittest)
+
 #result = result.loc[((result['right']<0.42*1.5)&(result['right']>0.42*0.5))|((result['left']<0.42*1.5)&(result['left']>0.42*0.5))]
 
-print(result['right'].mean())
-print(result['left'].mean())
+print(filename)
+print("ave a right step(sec): ",result['right'].mean())
+print("ave a left step(sec): ",result['left'].mean())
 
 #print(result)
